3e9839dce42fceab72be7e5bf2e15db76fc78ca3
Gelin Luo <greenlaw110@gmail.com>
2018-02-21 12:40:50 +1100

    persist password hash

--- src/main/java/meetup/handle_password/AppEntry.java
+++ src/main/java/meetup/handle_password/AppEntry.java
@@ -1,26 +1,29 @@
 package meetup.handle_password;
 
 import act.Act;
+import act.crypto.AppCrypto;
+import act.util.JsonView;
 import org.osgl.mvc.annotation.GetAction;
 import org.osgl.mvc.annotation.PutAction;
 
 import javax.inject.Inject;
 
 @SuppressWarnings("unused")
+@JsonView
 public class AppEntry {
 
     @Inject
     private User user;
 
     @PutAction("/user/password")
-    public String setPassword(String password) {
-        user.setPassword(password);
+    public String setPassword(String password, AppCrypto crypto) {
+        user.setPassword(crypto.passwordHash(password));
         return user.getPassword();
     }
 
     @GetAction("/user/password/verify")
-    public boolean verifyPassword(String password) {
-        return user.getPassword().equals(password);
+    public boolean verifyPassword(String password, AppCrypto crypto) {
+        return user.getPassword().equals(crypto.passwordHash(password));
     }
 
     public static void main(String[] args) throws Exception {
