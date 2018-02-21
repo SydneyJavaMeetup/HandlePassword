72c215b0dbbf7dc68a93e6e1ed12aa4e84520c89
Gelin Luo <greenlaw110@gmail.com>
2018-02-21 12:42:08 +1100

    fix verification failure

--- src/main/java/meetup/handle_password/AppEntry.java
+++ src/main/java/meetup/handle_password/AppEntry.java
@@ -15,15 +15,18 @@ public class AppEntry {
     @Inject
     private User user;
 
+    @Inject
+    private AppCrypto crypto;
+
     @PutAction("/user/password")
-    public String setPassword(String password, AppCrypto crypto) {
+    public String setPassword(String password) {
         user.setPassword(crypto.passwordHash(password));
         return user.getPassword();
     }
 
     @GetAction("/user/password/verify")
-    public boolean verifyPassword(String password, AppCrypto crypto) {
-        return user.getPassword().equals(crypto.passwordHash(password));
+    public boolean verifyPassword(String password) {
+        return crypto.verifyPassword(password, user.getPassword());
     }
 
     public static void main(String[] args) throws Exception {
